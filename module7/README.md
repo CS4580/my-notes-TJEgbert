e80a35a77c7bd8aaee048785bcc15691517ee436
Trevor Egbert <97637993+TJEgbert@users.noreply.github.com>
2024-11-04 10:15:42 -0700

    check profs notes

--- module7/README.md
+++ module7/README.md
@@ -1,5 +1,6 @@
 # Module 7 Notes: Metrics and Model Development
 
+### All code is in knn_analysis.py file
 
 ## Metrics
 
@@ -26,9 +27,9 @@ The Euclidean distance is the distance between points in `N-dimensional` space.
 
 Formula
 
-$
+$$
 d(p, q) = \sqrt{\sum_{i=1}^n (q_i - p_i)^2}
-$
+$$
 where:
 - $p = (p_1, p_2, \dots, p_n)$
 - $q = (q_1, q_2, \dots, q_n)$
@@ -39,3 +40,32 @@ Find the distance between these points:
 - y = (4, 4)
 
 Distance = 5.65685...
+
+```python
+# see
+def euclidean_similarity_normal()
+```
+
+### KNN with Jaccard Similarity Index
+Compare members of two individual sets to determine which members are `shared` and which are `distinct`.  The index measure the similarty the two sets.
+
+$$
+J(A, B) = \frac{|A \cap B|}{|A \cap B|}
+$$
+
+EX: $A = {1, 2, 3, 4}$ and $B = {3, 4, 5, 6}$ = $\frac {2}{6}$ or $0.33$
+
+```python
+# see
+def jaccard_similarity_normal()
+```
+### KNN with Weighted Jaccard Similarity Index
+The traditional Jaccard works well when doing `one-to-one` comparison between a category.  
+
+One solution is the `weighted` version.
+- Build a dictionary for `each genre` of the moives in our preferred list 
+
+```python
+# see
+def jaccard_similarity_weighted()
+```
--- module7/knn_analysis.py
+++ module7/knn_analysis.py
@@ -7,6 +7,7 @@ import get_data as gt
 # Constants
 K = 10  # number of closes matches
 BASE_CASE_ID = 88763  # IMDB_id for 'Back to the Future'
+BASE_YEAR = 1985
 
 
 def metric_stub(base_case_value, comparator_value):
@@ -17,14 +18,23 @@ def print_top_k(df, sorted_value, comparison_type):
     print(f'Top {K} closest matches by {comparison_type}')
     counter = 1
     for idx, row in df.head(K).iterrows():
-        print(f"""Top {counter} match: [{idx}:{row['year']} {row['title']}, {
-              row['genres']}, {row[sorted_value]}]""")
+        print(f"""Top {counter} match: [{idx}]:{row['year']} {row['title']}, {
+              row['genres']}, [{row[sorted_value]}]""")
         counter += 1
 
 
 def euclidean_distance(base_case_year: int, comparator_year: int):
     return abs(base_case_year - comparator_year)
 
+
+def jaccard_similarity_normal(base_case_genres: str, comparator_genres: str):
+    base_genres_set = set(base_case_genres.split(';'))
+    comparator_genres_set = set(comparator_genres.split(';'))
+    numerator = len(base_genres_set.intersection(comparator_genres_set))
+    denominator = len(base_genres_set.union(comparator_genres_set))
+    return float(numerator)/float(denominator)
+
+    
 
 def knn_analysis_driver(data_df, base_case, comparison_type, metric_func, sorted_value='metric'):
     # WIP: Create df of filter data
@@ -33,8 +43,13 @@ def knn_analysis_driver(data_df, base_case, comparison_type, metric_func, sorted
         # Uses the  input function and passes parementers here
         lambda x: metric_func(base_case[comparison_type], x))
     # Sort return values fromm function stub
-    sorted_df = df.sort_values(by=sorted_value)
-    sorted_df.drop(BASE_CASE_ID, inplace=True)  # drop base case
+    # jaccard needs to be sorting decending
+    if 'jaccard' in metric_func.__name__:
+        sorted_df = df.sort_values(by=sorted_value, ascending=False)
+    else:
+        sorted_df = df.sort_values(by=sorted_value) # default is ascending = True
+    sorted_df.drop(BASE_CASE_ID, inplace=True)  # drop base case 
+    # Check later whats wrong ----------------------------------------------------
     # print(sorted_df['title'].head(K)) # Top ten values
     print_top_k(sorted_df, sorted_value, comparison_type)
 
@@ -63,6 +78,14 @@ def main():
     print(f'\nTask 4: KNN Analysis with Euclidean Distance')
     knn_analysis_driver(data_df=data, base_case=base_case,
                         comparison_type='year', metric_func=euclidean_distance, sorted_value='euclidean_distance')
+
+    # Task 5: Jaccard Similarity
+    print(f'\nTask 4: KNN Analysis with Jarracrd Similarity Normal')
+    data = data[data['year'] >= BASE_CASE_ID] # filter
+    knn_analysis_driver(data_df=data, base_case=base_case,
+                        comparison_type='genres', metric_func=jaccard_similarity_normal, sorted_value='jaccard_similarity')
+    
+    
 
 
 if __name__ == '__main__':
